0271c79d87c85a67afbfd74ab5dc1ca05a8be87d
Sean Krueger <skrueger2270@gmail.com>
2025-11-15 14:50:37 -0600

    Add sidebar

---
+++ src/app/user/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default async function Page() {
+    return (
+        <div className="ml-5">
+            <h1>Test</h1>
+            <Button variant={'destructive'} size={'lg'}>Click me</Button>
+        </div>
+    )
+}
