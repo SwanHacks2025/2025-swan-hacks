de2667a37623d3862447db308e400f42245360ea
Josh A <josh@rarceo.com>
2025-11-15 13:34:49 -0600

    Started firebase implemntation

---
+++ src/lib/firebaseAdmin.ts
@@ -0,0 +1,18 @@
+// lib/firebaseAdmin.ts
+import admin from "firebase-admin";
+
+const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n");
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey,
+    }),
+    databaseURL: process.env.FIREBASE_DATABASE_URL, // optional
+  });
+}
+
+export const adminDb = admin.firestore();        // or admin.database()
+export const adminAuth = admin.auth();
